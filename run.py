fa6f94e8e09adea0ff1cda97e739d07c688ce756
mensheviks <mensheviks@126.com>
2017-08-03 21:48:35 +0800

    Second commit

--- run.py
+++ run.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/zhefeng')
-def hello_world():
-	return render_template('zhefeng-index.html')
+@app.route('/<string:student_number>/index')
+def show_index(student_number):
+	if student_number == "A10552" or student_number == "A10294":
+		return render_template(str(student_number) + '/index.html')
+	else:
+		return "Page not found"
 
 if __name__ == '__main__':
     app.run(debug = True)
